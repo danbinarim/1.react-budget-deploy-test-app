cfee5d293c23b508a9287c6ad9d2745eabee5938
kong <kong@kongui-MacBookPro.local>
2024-10-12 05:30:34 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,129 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component, useState } from "react";
+import "./App.css";
+import ExpenseForm from "./components/ExpenseForm";
+import ExpenseList from "./components/ExpenseList";
+import Alert from "./components/Alert";
 
-function App() {
+const App = () => {
+  const [charge, setCharge] = useState("");
+  const [id, setId] = useState("");
+  const [amount, setAmount] = useState(0);
+  const [edit, setEdit] = useState(false);
+
+  const [alert, setAlert] = useState({
+    show: false,
+  });
+
+  const [expenses, setExpenses] = useState([
+    { id: 1, charge: "렌트비", amount: 2000 },
+    { id: 2, charge: "교통비", amount: 400 },
+    { id: 3, charge: "식비", amount: 1200 },
+  ]);
+
+  const clearItems = () => {
+    setExpenses([]);
+  };
+
+  const handleCharge = (e) => {
+    console.log(e.target.value);
+    setCharge(e.target.value);
+  };
+
+  const handleAmount = (e) => {
+    console.log(e.target.valueAsNumber);
+    setAmount(e.target.valueAsNumber);
+  };
+
+  const handleDelete = (id) => {
+    const newExpenses = expenses.filter((expense) => expense.id !== id);
+    setExpenses(newExpenses);
+    handleAlert({
+      type: "danger",
+      text: "아이템이 삭제되었습니다.",
+    });
+  };
+
+  const handleAlert = ({ type, text }) => {
+    setAlert({ show: true, type, text });
+    setTimeout(() => {
+      setAlert({ show: false });
+    }, 3000);
+  };
+
+  const handleEdit = (id) => {
+    const expense = expenses.find((expense) => expense.id === id);
+    console.log(expense);
+
+    const { charge, amount } = expense;
+    setId(id);
+    setCharge(charge);
+    setAmount(amount);
+    setEdit(true);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (charge !== "" && amount > 0) {
+      if (edit) {
+        const newExpenses = expenses.map((item) => {
+          return item.id === id ? { ...item, charge, amount } : item;
+        });
+        setExpenses(newExpenses);
+        setEdit(false);
+        handleAlert({ type: "success", text: "아이템이 수정되었습니다." });
+      } else {
+        const newExpense = {
+          id: crypto.randomUUID,
+          charge,
+          amount,
+        };
+        setExpenses([...expenses, newExpense]);
+        handleAlert({ type: "success", text: "아이템이 생성되었습니다." });
+      }
+      setCharge("");
+      setAmount(0);
+    } else {
+      handleAlert({
+        type: "danger",
+        text: "charge는 빈 값일 수 없으며 amount는 0보다 커야 합니다.",
+      });
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <main className="main-container">
+      {alert.show ? <Alert type={alert.type} text={alert.text} /> : null}
+      <h1>예산 계산기</h1>
+      <div style={{ width: "100%", backgroundColor: "white", padding: "1rem" }}>
+        <ExpenseForm
+          handleCharge={handleCharge}
+          handleAmount={handleAmount}
+          charge={charge}
+          amount={amount}
+          handleSubmit={handleSubmit}
+          edit={edit}
+        />
+      </div>
+      <div style={{ width: "100%", backgroundColor: "white", padding: "1rem" }}>
+        <ExpenseList
+          expenses={expenses}
+          handleDelete={handleDelete}
+          handleEdit={handleEdit}
+          clearItems={clearItems}
+        />
+      </div>
+      <div
+        style={{ display: "flex", justifyContent: "end", marginTop: "1rem" }}
+      >
+        <p style={{ fontSize: "2rem" }}>
+          총지출:
+          <span>
+            {expenses.reduce((acc, curr) => (acc += curr.amount), 0)} 원
+          </span>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      </div>
+    </main>
   );
-}
+};
 
 export default App;
